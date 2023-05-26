da5fe1360961c19c6768f2731fe33e2a6ea52322
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-05-26 13:32:14 +0100

    add multiplayer game storyboard to utilize mupliplayerScore storyboard

---
+++ StoryboardComposition/StoryboardComposition/MultiplayerScoreViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MultiplayerScoreViewController.swift
+//  StoryboardComposition
+//
+//  Created by Yevhenii Veretennikov on 26.05.2023.
+//
+
+import UIKit
+
+final class MultiplayerScoreViewController: UIViewController {
+    var playerOne: PlayerScoreViewController?
+    var playerTwo: PlayerScoreViewController?
+}
