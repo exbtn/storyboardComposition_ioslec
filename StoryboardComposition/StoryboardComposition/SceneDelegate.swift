e4ad471c50ff2fb5691a46eb912be9b436e4d512
Yevhenii Veretennikov <yevheniiveretennikov@Yevheniis-MacBook-Pro.local>
2023-05-26 09:16:51 +0100

    add initial single player storyboard

---
+++ StoryboardComposition/StoryboardComposition/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  StoryboardComposition
+//
+//  Created by Yevhenii Veretennikov on 26.05.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
