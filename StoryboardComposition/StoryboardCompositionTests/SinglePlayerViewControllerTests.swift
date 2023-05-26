6aad72997b9a81e1ca468fcc930c73db3ff57993
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-05-26 10:05:45 +0100

    add name and score label tests

--- StoryboardComposition/StoryboardCompositionTests/SinglePlayerViewControllerTests.swift
+++ StoryboardComposition/StoryboardCompositionTests/SinglePlayerViewControllerTests.swift
@@ -10,18 +10,35 @@ import XCTest
 
 class SinglePlayerViewControllerTests: XCTestCase {
     
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    
     func test_mainStoryboard_initialViewControllerIsNotNil() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateInitialViewController()
         
         XCTAssertNotNil(vc)
     }
     
     func test_mainStoryboard_initialViewControllerIsSinglePlayerViewController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateInitialViewController() as? SinglePlayerViewController
         
         XCTAssertNotNil(vc)
     }
     
+    func test_SinglePlayerViewController_name_isSettable() {
+        let vc = storyboard.instantiateInitialViewController() as! SinglePlayerViewController
+        _ = vc.view
+        
+        vc.name = "a name"
+        
+        XCTAssertEqual(vc.name, "a name")
+    }
+    
+    func test_SinglePlayerViewController_score_isSettable() {
+        let vc = storyboard.instantiateInitialViewController() as! SinglePlayerViewController
+        _ = vc.view
+        
+        vc.score = "a score"
+        
+        XCTAssertEqual(vc.score, "a score")
+    }
 }
