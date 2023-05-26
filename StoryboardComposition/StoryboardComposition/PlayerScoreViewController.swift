16d4f662232636aa1719f65cc9bb104f20799276
Yevhenii Veretennikov <thethunderwrath@gmail.com>
2023-05-26 10:59:40 +0100

    move player score to separate storyboards

---
+++ StoryboardComposition/StoryboardComposition/PlayerScoreViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerScoreViewController.swift
+//  StoryboardComposition
+//
+//  Created by Yevhenii Veretennikov on 26.05.2023.
+//
+
+import UIKit
+
+final class PlayerScoreViewController: UIViewController {
+    @IBOutlet private weak var nameLabel: UILabel?
+    @IBOutlet private weak var scoreLabel: UILabel?
+    
+    var name: String? {
+        get { nameLabel?.text }
+        set { nameLabel?.text = newValue }
+    }
+    
+    var score: String? {
+        get { scoreLabel?.text }
+        set { scoreLabel?.text = newValue }
+    }
+}
--- StoryboardComposition/StoryboardComposition/SinglePlayerViewController.swift
+++ StoryboardComposition/StoryboardComposition/SinglePlayerViewController.swift
@@ -8,16 +8,5 @@
 import UIKit
 
 final class SinglePlayerViewController: UIViewController {
-    @IBOutlet private weak var nameLabel: UILabel?
-    @IBOutlet private weak var scoreLabel: UILabel?
-    
-    var name: String? {
-        get { nameLabel?.text }
-        set { nameLabel?.text = newValue }
-    }
-    
-    var score: String? {
-        get { scoreLabel?.text }
-        set { scoreLabel?.text = newValue }
-    }
+    @IBOutlet private(set) weak var player: PlayerScoreViewController?
 }
